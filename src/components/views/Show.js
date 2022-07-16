1e2389dcb9fd32093ecf0954e703631d0e4b5267
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-07-16 13:36:25 -0400

    delete bug in showjs

--- src/components/views/Show.js
+++ src/components/views/Show.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { collection, getDocs, deleteDoc, doc, query, orderBy, where } from 'firebase/firestore'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Show.css';
 import { db } from '../../firebaseConfig/firebase'
 import LastEdition from '../utils/LastEdition';
